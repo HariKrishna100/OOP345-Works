5a8511ce3e679f5a105ed87945adaa34fc114d0b
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-03-16 13:12:09 -0400

    STL Algorithm

---
+++ WS07/part 2/Bakery.cpp
@@ -0,0 +1,104 @@
+/*
+*****************************************************************************
+                          w7p2
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NCC
+Date       : 3/172022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include "Bakery.h"
+using namespace std;
+namespace sdds {
+   Bakery::Bakery(const char* filename) {
+      ifstream file(filename);
+      string data{};
+
+      if (!file) throw "Incorrect file";
+      
+      while (getline(file, data)) {
+         BakedGood bItems{};
+         string baketyp{};
+
+         baketyp = data.substr(0, data.find(" "));
+         transform(baketyp.begin(), baketyp.end(), baketyp.begin(), ::toupper);
+         bItems.m_type = clrSpace(baketyp) == "BREAD" ? BakedType::BREAD : BakedType::PASTERY;
+         bItems.m_desc = clrSpace(data.substr(8, 20));
+         bItems.m_expDay = stoi(clrSpace(data.substr(28, 14)));
+         bItems.m_stock = stoi(clrSpace(data.substr(42, 8)));
+         bItems.m_price = stod(clrSpace(data.substr(50, 6)));
+
+         m_bakedItems.push_back(bItems);
+      }
+      file.close();
+   }
+
+   void Bakery::showGoods(ostream& os) const {
+      for_each(m_bakedItems.begin(), m_bakedItems.end(), [&os](const BakedGood& B) { os << B; });
+   }
+
+   void Bakery::sortBakery(string str) {
+      transform(str.begin(), str.end(), str.begin(), ::toupper);
+      sort(m_bakedItems.begin(), m_bakedItems.end(), [str](const BakedGood& Bk1, const BakedGood Bk2) {
+         bool success{};
+         (str == "DESCRIPTION") ? success = Bk1.m_desc < Bk2.m_desc : success;
+         (str == "SHELF") ? success = Bk1.m_expDay < Bk2.m_expDay : success;
+         (str == "STOCK") ? success = Bk1.m_stock < Bk2.m_stock : success;
+         (str == "PRICE") ? success = Bk1.m_price < Bk2.m_price : success;
+      });
+   }
+
+   vector<BakedGood> Bakery::combine(Bakery& Bk) {
+      vector<BakedGood> combined(m_bakedItems.size() + Bk.m_bakedItems.size());
+      sortBakery("Price");
+      Bk.sortBakery("Price");
+
+      merge(m_bakedItems.begin(), m_bakedItems.end(), Bk.m_bakedItems.begin(), Bk.m_bakedItems.end(), combine.begin(), [](const BakedGood& Bg1, const BakedGood& Bg2) {
+         return Bg1.m_price < Bg2.m_price;
+      });
+      return combined;
+   }
+
+   bool Bakery::inStock(const string str, const BakedType& Bt) const {
+      return any_of(m_bakedItems.begin(), m_bakedItems.end(), [str, Bt](const BakedGood& Bg) {
+         return Bg.m_desc == str && Bg.m_type == Bt;
+      });
+   }
+
+   list<BakedGood> Bakery::outOfStock(BakedType Bt) const {
+      list<BakedGood> outOfStk;
+      copy_if(m_bakedItems.begin(), m_bakedItems.end(), back_inserter(outOfStk), [Bt](const BakedGood& Bg) {
+         return Bg.m_type == Bt && Bg.m_stock == 0;
+      });
+
+      outOfStk.sort([](const BakedGood& Bg, const BakedGood& Bg2) {
+         return Bg.m_price < Bg2.m_price;
+      });
+      return outOfStk;
+   }
+
+   ostream& operator<<(ostream& out, const BakedGood& b) {
+      out << "* " << left << setw(10) << (b.m_type == BakedType::BREAD ? "Bread" : "Pastry");
+      out << " * " << left << setw(20) << b.m_desc;
+      out << " * " << left << setw(5) << b.m_expDay;
+      out << " * " << left << setw(5) << b.m_stock;
+      out << " * " << right << fixed << setprecision(2) << setw(8) << b.m_price;
+      out << " * " << endl;
+      return out;
+   }
+
+   string Bakery::clrSpace(const string& str) {
+      auto first = find_if_not(str.begin(), str.end(), [](auto c) { return isspace(c); });
+      auto last = find_if_not(str.rbegin(), str.rend(), [](auto c) { return isspace(c); }).base();
+      return (first >= last) ? "" : string(first, last);
+   }
+}
---
+++ WS07/part 2/Bakery.h
@@ -0,0 +1,47 @@
+/*
+*****************************************************************************
+                          w7p2
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NCC
+Date       : 3/172022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+#ifndef SDDS_BAKERY_H_
+#define SDDS_BAKERY_H_
+#include <iostream>
+#include <list>
+#include <vector>
+namespace sdds {
+   enum class BakedType {
+      BREAD, PASTERY
+   };
+
+   struct BakedGood {
+      BakedType m_type{};
+      std::string m_desc{};
+      int m_expDay{0};
+      int m_stock{0};
+      double m_price{0.0};
+   };
+
+   class Bakery {
+      std::vector<BakedGood> m_bakedItems;
+   public:
+      Bakery() = default;
+      Bakery(const char* filename);
+      void showGoods(std::ostream& os) const;
+      void showGoods(std::ostream& os) const;
+      void sortBakery(std::string str);
+      std::vector<BakedGood> combine(Bakery& Bk);
+      bool inStock(const std::string str, const BakedType& Bt) const;
+      std::list<BakedGood> outOfStock(BakedType Bt) const;
+      std::string clrSpace(const std::string& str);
+   };
+   std::ostream& operator<<(std::ostream& out, const BakedGood& b);
+}
+#endif // !SDDS_BAKERY_H_
