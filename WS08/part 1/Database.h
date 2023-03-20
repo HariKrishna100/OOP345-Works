608d05875d0e16ebc48f1a502ea4a621f206a735
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-03-20 12:54:49 -0400

    Added enum class and database class

--- WS08/part 1/Database.h
+++ WS08/part 1/Database.h
@@ -2,6 +2,15 @@
 #define SDDS_DATABASE_H_
 #include <iostream>
 namespace sdds {
+   enum class Err_Status
+   {
+      Err_Success,
+      Err_NotFound,
+      Err_OutOfMemory,
+   };
+   class Database {
+   public:
 
+   };
 }
 #endif // !SDDS_DATABASE_H_
