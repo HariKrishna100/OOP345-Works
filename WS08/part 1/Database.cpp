9c18c89f4771cdceeb5a103eaf3ef82ed1c58307
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-03-23 17:12:29 -0400

    file read

--- WS08/part 1/Database.cpp
+++ WS08/part 1/Database.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <iomanip>
 #include "Database.h"
 using namespace std;
 namespace sdds {
    Database::Database(const std::string& filename) {
+      m_filename = filename;
+      ifstream file(filename);
+      string key{}, value{};
 
+      cout << "[" << this << "]" << " Database(const std::string&)" << endl;
+      while (file >> key >> value) {
+         replace(key.begin(), key.end(), '-', ' ');
+         SetValue(key, value);
+      }
+      file.close();
    }
 
    shared_ptr<Database> Database::getInstance(const std::string& filename) {
-
+      if(m_database)
    }
 
    Err_Status Database::GetValue(const std::string& key, std::string& value) {
