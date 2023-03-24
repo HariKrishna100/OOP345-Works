0b91363545f1e75c784af5b7397e37fe1320d932
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-03-24 11:21:05 -0400

    Err_NotFound

--- WS08/part 1/Database.cpp
+++ WS08/part 1/Database.cpp
@@ -18,19 +18,40 @@ namespace sdds {
       file.close();
    }
 
-   shared_ptr<Database> Database::getInstance(const std::string& filename) {
-      if(m_database)
+   std::shared_ptr<Database> Database::getInstance(const std::string& filename) {
+      return (m_database) ? m_database : m_database = std::shared_ptr<Database>(new Database(filename));
    }
 
    Err_Status Database::GetValue(const std::string& key, std::string& value) {
-      
+      Err_Status status{ Err_Status::Err_NotFound };
+      int flag = 0;
+      for (int i = 0; i < m_noOfEntry && flag == 0; i++){
+         if (m_key[i] == key) 
+            m_value[i] = value;
+         status = { Err_Status::Err_Success };
+         flag = 1;
+      }
+      return status;
    }
 
    Err_Status Database::SetValue(const std::string& key, const std::string& value) {
-     
+      Err_Status status{ Err_Status::Err_OutOfMemory };
+      if (m_noOfEntry < m_maxEntry) {
+         status = { Err_Status::Err_Success };
+         m_noOfEntry++;
+         m_key[m_noOfEntry] = key;
+         m_value[m_noOfEntry] = value;
+      }
+      return status;
    }
 
    Database::~Database() {
-
+      for (int i = 0; i < m_noOfEntry; i++) {
+         cout << "[" << this << "]" << "~Database()";
+         ofstream databaseBackup(m_filename + ".bkp.txt");
+         for (int i = 0; i < m_noOfEntry; i++) {
+            databaseBackup << setw(25) << left << m_key[i] << " -> " << m_value[i] << endl;
+         }
+      }
    }
 }
